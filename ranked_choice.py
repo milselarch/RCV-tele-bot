24c44f0bc66db4d972c92990b436fd58a0b03f66
milselarch <milselarch@gmail.com>
2023-10-11 20:06:31 +0800

    add support for 0, None votes in ranked choice algorithm

--- ranked_choice.py
+++ ranked_choice.py
@@ -3,6 +3,9 @@ import itertools
 
 from typing import List, Tuple
 
+ZERO_VOTE = 0
+NULL_VOTE = None
+
 
 def ranked_choice_vote(
     ranked_votes: List[List[int]], num_voters: int = None
@@ -14,6 +17,10 @@ def ranked_choice_vote(
     each ranked vote is a list of candidate preferences
     first choice is rightmost (highest index) element in list
     last choice is leftmost (lowest index) element in list
+
+    special vote ranking values:
+    ZERO_VOTE (0) - give the vote to none of the options in the poll
+    NULL_VOTE (None) - remove the voter from the poll
     :return:
     """
     ranked_votes = copy.deepcopy(ranked_votes)
@@ -21,8 +28,17 @@ def ranked_choice_vote(
     if num_voters is None:
         num_voters = len(ranked_votes)
 
+    # number of voters who have not voided their votes
+    effective_num_voters = num_voters
     assert num_voters >= len(ranked_votes)
     unique_candidates = set(itertools.chain(*ranked_votes))
+
+    # remove 0 and None votes from unique candidates
+    if ZERO_VOTE in unique_candidates:
+        unique_candidates.remove(ZERO_VOTE)
+    if NULL_VOTE in unique_candidates:
+        unique_candidates.remove(NULL_VOTE)
+
     candidate_votes_map = {
         candidate: 0 for candidate in unique_candidates
     }
@@ -31,7 +47,18 @@ def ranked_choice_vote(
     # count how many votes each candidate got
     # using the first-choice votes of each voter
     for ranked_vote in ranked_votes:
-        candidate_votes_map[ranked_vote[-1]] += 1
+        top_choice = ranked_vote[-1]
+
+        if top_choice == ZERO_VOTE:
+            # 0 means voter has chosen to vote for no one
+            pass
+        elif top_choice == NULL_VOTE:
+            # None means the voter has chosen to remove
+            # himself from the poll
+            effective_num_voters -= 1
+        else:
+            assert top_choice in unique_candidates
+            candidate_votes_map[top_choice] += 1
 
     while winner is None:
         winner = None
@@ -52,10 +79,8 @@ def ranked_choice_vote(
         if len(candidate_vote_counts) == 2:
             # if there are two 1st choice candidates have the exact
             # same number of votes and there are no other 1st choice
-            # candidates, then we scan the number of candidate
-            # occurrences in the 2nd, 3rd choices to tiebreak
+            # candidates, then we declare the vote to be tied
             if lowest_votes == highest_votes:
-                # just say it's not conclusive for now
                 return None
 
         weakest_candidates = []
@@ -65,11 +90,13 @@ def ranked_choice_vote(
             if candidate_votes == lowest_votes:
                 weakest_candidates.append(candidate)
 
-            if candidate_votes > num_voters / 2:
+            if candidate_votes > effective_num_voters / 2:
                 winner = candidate
                 break
 
         print('dropping candidates', weakest_candidates)
+        if winner is not None:
+            break
 
         vote_transfers = 0
         # vote transfer to next choice for worst performing candidate(s)
@@ -84,17 +111,27 @@ def ranked_choice_vote(
             # get active top choice candidate
             # for the current ranked choice vote
             top_choice = ranked_vote[-1]
+            assert top_choice in candidate_votes_map
 
             if top_choice in weakest_candidates:
                 # remove the top candidate from the current
                 # ranked choice vote if aforementioned candidate
                 # is the weakest candidate
                 ranked_vote.pop()
-
+                # get next choice that the voter wants
                 next_choice = ranked_vote[-1]
-                candidate_votes_map[top_choice] -= 1
-                candidate_votes_map[next_choice] += 1
-                vote_transfers += 1
+
+                if next_choice == ZERO_VOTE:
+                    candidate_votes_map[top_choice] -= 1
+                    vote_transfers += 1
+                elif next_choice == NULL_VOTE:
+                    candidate_votes_map[top_choice] -= 1
+                    effective_num_voters -= 1
+                    vote_transfers += 1
+                else:
+                    candidate_votes_map[top_choice] -= 1
+                    candidate_votes_map[next_choice] += 1
+                    vote_transfers += 1
 
         if vote_transfers == 0:
             # no voter had their candidate preference shifted
