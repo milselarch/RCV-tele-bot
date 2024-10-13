601a2df42958d990c068060ad4f18c9972f38d37
milselarch <milselarch@gmail.com>
2024-10-13 16:43:34 +0800

    add workflow actions

--- tests/enum_test.py
+++ tests/enum_test.py
@@ -1,5 +1,6 @@
 import unittest
-import ParentImport
+# noinspection PyUnresolvedReferences
+# import ParentImport
 
 from SpecialVotes import SpecialVotes as SpecV
 
@@ -15,13 +16,13 @@ class TestSpecialVotes(unittest.TestCase):
 
     def test_to_string(self):
         # Test the to_string method
-        self.assertEqual(SpecV.WITHHOLD_VOTE.to_string(), '0')
-        self.assertEqual(SpecV.ABSTAIN_VOTE.to_string(), 'nil')
+        self.assertEqual(SpecV.WITHHOLD_VOTE.to_string(), 'withhold')
+        self.assertEqual(SpecV.ABSTAIN_VOTE.to_string(), 'abstain')
 
     def test_from_string(self):
         # Test the from_string method
-        self.assertEqual(SpecV.from_string('0'), SpecV.WITHHOLD_VOTE)
-        self.assertEqual(SpecV.from_string('nil'), SpecV.ABSTAIN_VOTE)
+        self.assertEqual(SpecV.from_string('withhold'), SpecV.WITHHOLD_VOTE)
+        self.assertEqual(SpecV.from_string('abstain'), SpecV.ABSTAIN_VOTE)
         with self.assertRaises(ValueError):
             SpecV.from_string('invalid')
 
@@ -34,8 +35,9 @@ class TestSpecialVotes(unittest.TestCase):
     def test_string_maps(self):
         # Test if string maps are correctly set
         self.assertEqual(
-            SpecV.get_string_map(),
-            {SpecV.WITHHOLD_VOTE: '0', SpecV.ABSTAIN_VOTE: 'nil'}
+            SpecV.get_string_map(), {
+                SpecV.WITHHOLD_VOTE: 'withhold', SpecV.ABSTAIN_VOTE: 'abstain'
+            }
         )
 
 
--- tests/test_ranked_vote.py
+++ tests/test_ranked_vote.py
@@ -1,8 +1,8 @@
 import unittest
-import ParentImport
+# noinspection PyUnresolvedReferences
+# import ParentImport
+import ranked_choice_vote
 
-from RankedChoice import ranked_choice_vote
-from RankedVote import RankedVote
 from SpecialVotes import SpecialVotes
 
 
@@ -16,98 +16,148 @@ class TestRankedChoiceVote(unittest.TestCase):
     """
     def test_basic_scenario(self):
         # Basic test with predefined votes
+        votes_aggregator = ranked_choice_vote.VotesAggregator()
         votes = [
-            RankedVote([1, 2, 3, 4]),
-            RankedVote([1, 2, 3]),
-            RankedVote([3]),
-            RankedVote([3, 2, 4]),
-            RankedVote([4, 1])
+            [1, 2, 3, 4],
+            [1, 2, 3],
+            [3],
+            [3, 2, 4],
+            [4, 1]
         ]
-        result = ranked_choice_vote(votes, verbose=self.verbose)
+
+        for vote_idx in range(len(votes)):
+            vote_rankings = votes[vote_idx]
+            for vote_ranking in vote_rankings:
+                votes_aggregator.insert_vote_ranking(vote_idx, vote_ranking)
+
+        votes_aggregator.flush_votes()
+        winner = votes_aggregator.determine_winner()
         self.assertEqual(
-            result, 1,
+            winner, 1,
             "The winner should be candidate 1"
         )
 
     def test_simple_majority(self):
         # Basic test where there is a winner in round 1
+        votes_aggregator = ranked_choice_vote.VotesAggregator()
         votes = [
-            RankedVote([1, 2, 3, 4]),
-            RankedVote([1, 2, 3]),
-            RankedVote([3]),
-            RankedVote([3, 2, 4]),
-            RankedVote([1, 2])
+            [1, 2, 3, 4],
+            [1, 2, 3],
+            [3],
+            [3, 2, 4],
+            [1, 2]
         ]
-        result = ranked_choice_vote(votes, verbose=self.verbose)
+
+        for vote_idx in range(len(votes)):
+            vote_rankings = votes[vote_idx]
+            for vote_ranking in vote_rankings:
+                votes_aggregator.insert_vote_ranking(vote_idx, vote_ranking)
+
+        votes_aggregator.flush_votes()
+        winner = votes_aggregator.determine_winner()
         self.assertEqual(
-            result, 1,
+            winner, 1,
             "The winner should be candidate 1"
         )
 
     def test_tie_scenario(self):
         # Test for a tie
+        votes_aggregator = ranked_choice_vote.VotesAggregator()
         votes = [
-            RankedVote([1, 2]),
-            RankedVote([2, 1])
+            [1, 2],
+            [2, 1]
         ]
-        result = ranked_choice_vote(votes)
-        self.assertIsNone(result, "There should be a tie")
+        for vote_idx in range(len(votes)):
+            vote_rankings = votes[vote_idx]
+            for vote_ranking in vote_rankings:
+                votes_aggregator.insert_vote_ranking(vote_idx, vote_ranking)
+
+        votes_aggregator.flush_votes()
+        winner = votes_aggregator.determine_winner()
+        self.assertIsNone(winner, "There should be a tie")
 
     def test_zero_vote_end(self):
         # Test that a zero vote ends with no one winning
+        votes_aggregator = ranked_choice_vote.VotesAggregator()
         votes = [
-            RankedVote([1, SpecialVotes.WITHHOLD_VOTE]),
-            RankedVote([2, 1]),
-            RankedVote([3, 2]),
-            RankedVote([3])
+            [1, SpecialVotes.WITHHOLD_VOTE],
+            [2, 1],
+            [3, 2],
+            [3]
         ]
-        result = ranked_choice_vote(votes, verbose=self.verbose)
+        for vote_idx in range(len(votes)):
+            vote_rankings = votes[vote_idx]
+            for vote_ranking in vote_rankings:
+                votes_aggregator.insert_vote_ranking(vote_idx, vote_ranking)
+
+        votes_aggregator.flush_votes()
+        winner = votes_aggregator.determine_winner()
         self.assertEqual(
-            result, None,
+            winner, None,
             "Candidate 1's vote should not count, no one should win"
         )
 
     def test_zero_nil_votes_only(self):
         # Test that having only zero and nil votes ends with no one winning,
         # and also that there are no errors in computing the poll result
+        votes_aggregator = ranked_choice_vote.VotesAggregator()
         votes = [
-            RankedVote([SpecialVotes.WITHHOLD_VOTE]),
-            RankedVote([SpecialVotes.WITHHOLD_VOTE]),
-            RankedVote([SpecialVotes.WITHHOLD_VOTE]),
-            RankedVote([SpecialVotes.ABSTAIN_VOTE])
+            [SpecialVotes.WITHHOLD_VOTE],
+            [SpecialVotes.WITHHOLD_VOTE],
+            [SpecialVotes.WITHHOLD_VOTE],
+            [SpecialVotes.ABSTAIN_VOTE]
         ]
-        result = ranked_choice_vote(votes, verbose=self.verbose)
+        for vote_idx in range(len(votes)):
+            vote_rankings = votes[vote_idx]
+            for vote_ranking in vote_rankings:
+                votes_aggregator.insert_vote_ranking(vote_idx, vote_ranking)
+
+        votes_aggregator.flush_votes()
+        winner = votes_aggregator.determine_winner()
         self.assertEqual(
-            result, None,
+            winner, None,
             "No one should win if all votes were 0 or nil"
         )
 
     def test_null_vote_end(self):
         # Test that a null vote ends with someone winning
+        votes_aggregator = ranked_choice_vote.VotesAggregator()
         votes = [
-            RankedVote([1, SpecialVotes.ABSTAIN_VOTE]),
-            RankedVote([2, 1]),
-            RankedVote([3, 2]),
-            RankedVote([3])
+            [1, SpecialVotes.ABSTAIN_VOTE],
+            [2, 1],
+            [3, 2],
+            [3]
         ]
-        result = ranked_choice_vote(votes, verbose=self.verbose)
+        for vote_idx in range(len(votes)):
+            vote_rankings = votes[vote_idx]
+            for vote_ranking in vote_rankings:
+                votes_aggregator.insert_vote_ranking(vote_idx, vote_ranking)
+
+        votes_aggregator.flush_votes()
+        winner = votes_aggregator.determine_winner()
         self.assertEqual(
-            result, 3,
+            winner, 3,
             "Candidate 3's vote should not count, no one should win"
         )
 
     def test_majoritarian_rule(self):
+        votes_aggregator = ranked_choice_vote.VotesAggregator()
         votes = [
-            RankedVote([1, 6, 15]),
-            RankedVote([1, 2, 6, 15, 5, 4, 7, 3, 11]),
-            RankedVote([6, 15, 1, 11, 10, 16, 17, 8, 2, 3, 5, 7]),
-            RankedVote([9, 8, 6, 11, 13, 3, 1]),
-            RankedVote([13, 14, 16, 6, 3, 4, 5, 2, 1, 8, 9])
+            [1, 6, 15],
+            [1, 2, 6, 15, 5, 4, 7, 3, 11],
+            [6, 15, 1, 11, 10, 16, 17, 8, 2, 3, 5, 7],
+            [9, 8, 6, 11, 13, 3, 1],
+            [13, 14, 16, 6, 3, 4, 5, 2, 1, 8, 9]
         ]
+        for vote_idx in range(len(votes)):
+            vote_rankings = votes[vote_idx]
+            for vote_ranking in vote_rankings:
+                votes_aggregator.insert_vote_ranking(vote_idx, vote_ranking)
 
-        result = ranked_choice_vote(votes, verbose=self.verbose)
+        votes_aggregator.flush_votes()
+        winner = votes_aggregator.determine_winner()
         self.assertEqual(
-            result, 6,
+            winner, 6,
             "Candidate 6 should be the majoritarian winner"
         )
 
